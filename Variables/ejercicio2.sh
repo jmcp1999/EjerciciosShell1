98eea70faa38d7b9b977a28dceed7ad07191202d
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-05-17 10:50:31 +0200

    2

---
+++ Variables/ejercicio2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Mi actividad favorita es"
+read "actividad"
+echo "mi comida favorita es"
+read "comida"
+
+echo "Mi actividad favorita es $actividad. Mi comida favorita es $comida"
